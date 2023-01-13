d03b8fabcb43775c8e1795a98149e6799c5e8ce4
David Zmick <david@dpzmick.com>
2023-01-12 23:50:12 -0600

    Initial Commit

---
+++ GoGame/GoGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoGameApp.swift
+//  GoGame
+//
+//  Created by David Zmick on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GoGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
